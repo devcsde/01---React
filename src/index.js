8af370abc2edf2fc65b7cf5820e8efb3707d01c4
devcsde <c.scheidler@gmx.net>
2017-09-16 00:24:45 +0200

    maintainance

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import YTSearch from "youtube-api-search";
 import SearchBar from "./components/search_bar";
 import VideoList from "./components/video_list";
 import VideoDetail from "./components/video_detail";
-import API_KEY from "../config/config";
+// import API_KEY from "../config/config";
+
+const API_KEY = process.env.API_KEY;
 
 // create a component
 
