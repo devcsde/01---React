771c8a79c0dfbcb27d5cf83114f041fa0c4adc82
devcsde <c.scheidler@gmx.net>
2017-09-15 21:31:30 +0200

    Added components/views and youtube api

---
+++ src/components/search_bar.js
@@ -0,0 +1,24 @@
+import React, {Component} from "react";
+
+class SearchBar extends Component { // <- class based component
+  constructor(props) {
+    super(props);
+    this.state = { term: "" };
+  }
+  render() {
+    return (
+      <div className="search-bar">
+      <input
+        value={ this.state.term }
+        onChange={evt => this.onInputChange(evt.target.value)} />
+      </div>
+    );
+  }
+
+  onInputChange(term){
+    this.setState({term});
+    this.props.onSearchTermChange(term);
+  }
+}
+
+export default SearchBar;
---
+++ src/index.js
@@ -0,0 +1,46 @@
+import _ from "lodash";
+import React, {Component} from "react";
+import ReactDOM from "react-dom";
+import YTSearch from "youtube-api-search";
+
+import SearchBar from "./components/search_bar";
+import VideoList from "./components/video_list";
+import VideoDetail from "./components/video_detail";
+import API_KEY from "../config/config";
+
+// create a component
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+    this.videoSearch("dogs");
+  };
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term}, videos => {
+      this.setState({
+        videos,
+        selectedVideo: videos[0]
+      });
+    });
+  };
+  render() {
+    const videoSearch = _.debounce(term => {this.videoSearch(term)}, 300);
+
+    return (
+      <div>
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} />
+      </div>
+    );
+  };
+}
+
+// show on page in the DOM
+ReactDOM.render(<App/>, document.querySelector(".container"));
